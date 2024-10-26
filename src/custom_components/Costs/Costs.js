72025ed378e419c2ece19f2623ee890ff98e169c
Eeeeesss <167417@gmail.com>
2024-10-27 00:08:14 +0500

    filter

--- src/custom_components/Costs/Costs.js
+++ src/custom_components/Costs/Costs.js
@@ -17,18 +17,25 @@ const Costs = (props) => {
     }
     )
 
+    let costsContent = <p>В этом году расходов нет</p>
+
+    if (filteredCosts.length > 0) {
+        costsContent = filteredCosts.map((cost) => (
+            <CostItem
+                key={cost.id}
+                date={cost.date}
+                description={cost.description}
+                amount={cost.amount}
+            />
+        ))
+    }
+
     return (
         <div>
             <Card className="costs">
                 <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-                {filteredCosts.map((cost) => (
-                    <CostItem
-                        key={cost.id}
-                        date={cost.date}
-                        description={cost.description}
-                        amount={cost.amount}
-                    />
-                ))}
+                {costsContent}
+
             </Card>
         </div>
     )
